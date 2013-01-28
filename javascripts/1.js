f9b41843bab40fb696b101be18a5c725432ec93a
robinhwang <binbinhuangcsu@gmail.com>
2013-01-28 10:09:17 +0800

    more

--- javascripts/1.js
+++ javascripts/1.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   $("p").click(function() {
     alert("you click p  dom")
   });
+
+  $('ul.first').find('.foo')
+      .css('background-color', 'red')
+    .end().find('.bar')
+      .css('background-color', 'green')
+    .end();
 /*  $("div").click(function(){
     alert("you click div")
   })*/
