8b48f97553ce9fe576309d6fb371e45d6c3e4aed
robinhwang <binbinhuangcsu@gmail.com>
2013-01-28 12:08:34 +0800

    fun ! siblings

---
+++ javascripts/2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".article .thebody").hide();
+  $("#container .article ul")
+    .prepend("<li class='readbody'><a href='' title='Read the article'>Read Body</a></li>")
+    .click(function(e){
+      $(this).siblings('.thebody').toggle();
+      e.preventDefault();
+    });
+});
